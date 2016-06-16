cae708fe96de26a7c4fe6956e8b738b2f9c88fa5
Michael Nodzenski <michael.nodzenski@northwestern.edu>
2016-06-16 21:36:27 +0000

    Update mxtrmod to handle missing categories
    This update allows mixture modeling to proceed accurately in cases where
    a level of a categorical predictor is entirely missing metabolite data.
    mxtrmod now checks for and removes such categorical levels, and then
    calls function runMxtrmod to actually run the model.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metabomxtr@118673 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mxtrmod.R
+++ R/mxtrmod.R
@@ -1,6 +1,4 @@
-mxtrmod <-
-function(ynames,mxtrModel,Tvals=NULL,nNA=5,minProp=0.2,
-                                      method="BFGS",data,fullModel=NULL){
+mxtrmod <-function(ynames,mxtrModel,Tvals=NULL,nNA=5,minProp=0.2,method="BFGS",data,fullModel=NULL){
   ###############################################################################################
   # Determines optimized estimates for all parameters in a mixture model.  
   #
@@ -34,112 +32,119 @@ function(ynames,mxtrModel,Tvals=NULL,nNA=5,minProp=0.2,
   #  fullModel: A formula of the form ~x1+x2...|z1+z2..., where x's are the names of 
   #            covariates included in the discrete portion of the full model and z's are names
   #            of covariates included in the continuous portion. Input if the mxtrModel parameter
-  #            represents a reduced model.  
+  #            represents a reduced model. 
+  #  checkMissing: Should factor/character variables in the input mixture model be checked to determine 
+  #				   if sufficient data exists to estimate their effects? Defualts to TRUE. 
   # Returns:
   #   A data frame with optimized estimates for all parameters in the mixture model, the negative 
   #   log likelihood, the optimization method used, whether the algorithm converged, and the 
   #  number of observations used.  
   #################################################################################################
   
-  #Define a data frame containing the response variables
-  obsY<-yvals(data,ynames)  
-
-  #Make the model a Formula object 
-  mxtrModel1<-Formula(mxtrModel)
-
-  #Define the design matrix for the discrete portion of the model
-  xVars<-xdesign(data,mxtrModel1)
+  #first, determine the categorical variables in the model 
+  #this should be based on the full model 
+  if (!is.null(fullModel)){
   
-  #Define the design matrix for the continuous portion of the model 
-  zVars<-zdesign(data,mxtrModel1)
+	mxtrModelTarget<-fullModel
   
-  #If mxtrModel is a reduced model, make the fullModel parameter a formula object. 
-  #Then define a design matrices for the full version of the mixture model
-  #and merge the two design matrices together. 
-  if(!is.null(fullModel)){
-    fullMod<-Formula(fullModel)
-    xVarsFull<-xdesign(data,fullMod)
-    zVarsFull<-zdesign(data,fullMod)
-    allCovariates<-cbind(xVarsFull,zVarsFull)
-   } else {
-  #Otherwise, merge the original desgin matrices
-  	allCovariates<-cbind(xVars,zVars)
-   }
+  } else{
   
-  #Determine if any of the observations have missing covariate values
-  keep.these<-apply(allCovariates,1,function(x){all(!is.na(x))})
+	mxtrModelTarget<-mxtrModel
   
-  #Remove observations from the data frame of response variables that 
-  #do not have fully observed covariates
-  obsY<-obsY[keep.these, ,drop=FALSE]
- 
-  #Remove observations from the discrete design matrix that 
-  #do not have fully observed covariates
-  xVars<-xVars[keep.these, ,drop=FALSE]
-  
-  #Remove observations from the continuous design matrix that 
-  #do not have fully observed covariates
-  zVars<-zVars[keep.these, ,drop=FALSE]
-
-  #Run a mixture model on each response variable, to return optimized
-  #parameter estimates and the negative log likelihood 
-  result<-apply(obsY,2,function(obsY) {
-
-  #Determine if there are enough missing values to include the discrete 
-  #component of the mixture model.
-
-  includeDiscrete <- sum(is.na(obsY))>nNA
-
-  #Determine if there are enough non-missing values present to run the model 
-  if(sum(!is.na(obsY))>minProp*length(obsY)){
-
-  #If enough non-missing values are present, specify starting values for the model
-  myStart<-mxtrmodstart(obsY=obsY,xVars=xVars,zVars=zVars,includeDiscrete=includeDiscrete)
-
-  #Specify Tvals. By default, the function will use the minimum of the observed
-  #responses.
-  if(is.null(Tvals)){
-    Tvals <- rep(min(obsY,na.rm=TRUE),length(obsY))
   }
-  #Optimize the function.
-  optimResult <- optimx(myStart,
-                fn=mxtrmodLL,
-                obsY=obsY,xVars=xVars,zVars=zVars,Tvals=Tvals,includeDiscrete=includeDiscrete,
-		    method=method)
-
-  #Get the optimized parameter estimates. 
-  modelPars <- as.vector(coef(optimResult))
-
-  #Name the parameters 
-  if (dim(xVars)[2]==1){
-    xVarNames<-NULL
-  } else {
-  xVarNames<-paste("x_",colnames(xVars)[-1],sep="")
-  }
-  if (dim(zVars)[2]==1){
-    zVarNames<-NULL
-  } else {
-  zVarNames<-paste("z_",colnames(zVars)[-1],sep="")
-  }
-  #If the discrete component is included, name both continuous and discrete variables.
-  namedPars<- modelPars
-  if(includeDiscrete){
-    names(namedPars) <- c("xInt",xVarNames,"zInt",zVarNames,"sigma")
-
-  #Otherwise, only name the continuous variables. 
-  } else if(!includeDiscrete){
-    names(namedPars) <- c("zInt",zVarNames,"sigma")
-  }   
-  namedPars<-t(namedPars)
-   
-  #Calculate the negative log-likelihood of the model. 
-  negLL <- mxtrmodLL(modelPars,obsY=obsY,xVars=xVars,zVars=zVars,Tvals=Tvals,includeDiscrete=includeDiscrete)
   
-  #Combine the final results into a dataframe. 
-  ans <- data.frame(namedPars, method=rownames(optimResult),
-                    conv=optimResult$convcode,negLL=negLL,nObs=length(obsY))
-  } else ans <- NULL
-  ans
-})
-ldply(result,rbind)
-}
+  #Make the model a Formula object   
+  mxtrModelF<-Formula(mxtrModelTarget)
+  
+  #in the input data are expression sets, convert to data frames 
+  if (class(data)=="ExpressionSet"){
+
+	  data<-data.frame(cbind(t(exprs(data)), pData(data)))
+
+  }
+
+  #identify categorical variables in model 
+  model.vars<-all.vars(mxtrModelF)
+  model.var.classes<-sapply(model.vars,function(x){ class(data[ ,x])})
+  cat.vars<-names(model.var.classes)[model.var.classes %in% c("factor", "character")]
+  
+  #determine whether any of the levels of categorical variables are entirely missing
+  missing.levels.check<-lapply(ynames, anyMissingLevels, cat.vars=cat.vars, dataset=data)
+  names(missing.levels.check)<-ynames
+  any.missing.levels<-sapply(missing.levels.check, any)
+  missing.level.vars<-names(any.missing.levels)[any.missing.levels]
+  no.missing.level.vars<-names(any.missing.levels)[!any.missing.levels]
+
+  #if there are any variables entirely missing for at least one level of the categorical variables, 
+  #remove those levels from the data and run a mixture model
+  if (length(missing.level.vars)>0){
+  
+	missing.level.res.list<-lapply(missing.level.vars, function(var.name){
+
+		#identify actual levels that are missing 
+		missing.level.list<-idMissingLevels(var.name, missing.levels.check, data)
+		
+		#remove those levels 
+		clean.data<-removeMissingLevels(var.name, missing.level.list, data)
+		
+		#if the Tvals are provided, and they are batch Tvals, make sure all batches are present 
+		if (any(grep("batch", names(Tvals)))){
+		
+			#get the name of the batch variable
+			batch.varname<-strsplit(names(Tvals)[1], " ")[[1]][2]
+			
+			#if batch is missing a level, then remove the corresponding Tvals
+			if (batch.varname %in% names(missing.level.list)){
+			
+				batch.missing.levels<-missing.level.list[[batch.varname]]
+				batch.missing.tval.names<-paste("batch", batch.varname, batch.missing.levels)
+				Tvals<-Tvals[! names(Tvals) %in% batch.missing.tval.names]
+			
+			}
+		
+		}
+		
+		#run mixture model 
+		mixmod.results<-runMxtrmod(ynames=var.name, mxtrModel=mxtrModel, Tvals=Tvals, nNA=nNA, minProp=minProp, method=method, data=clean.data, fullModel=fullModel)
+		
+		#add warning note about excluded levels
+		warning.note<-c()
+		for (element in names(missing.level.list)){
+		
+			missing.values<-missing.level.list[[element]]
+			missing.value.note<-paste(element, paste(missing.values, collapse=", "), sep=":")
+			warning.note<-c(warning.note, missing.value.note)
+		
+		}
+		mixmod.results$missing_levels_warning<-paste(warning.note, collapse=", ")
+		return(mixmod.results)
+		
+	})
+	
+  }  
+  
+  if (length(no.missing.level.vars)>0){
+  
+  
+	no.missing.results<-runMxtrmod(ynames=no.missing.level.vars, mxtrModel=mxtrModel, Tvals=Tvals, nNA=nNA, minProp=minProp, method=method, data=data, fullModel=fullModel)
+  
+  }
+  
+  #put together final results table 
+  if (exists('no.missing.results') & exists('missing.level.res.list')){
+  
+	final.result<-rbind.fill(c(list(no.missing.results), missing.level.res.list))  
+  
+  } else if (exists('missing.level.res.list')){
+    
+	final.result<-rbind.fill(missing.level.res.list) 
+  
+  } else if (exists('no.missing.results')){
+  
+	final.result<-no.missing.results
+  
+  }
+  
+  return(final.result)
+  
+ }
