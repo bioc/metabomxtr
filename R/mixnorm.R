ef85bcaf5ef966782199fabad6c1671ee0d33ca3
Michael Nodzenski <michael.nodzenski@northwestern.edu>
2016-12-14 16:28:59 +0000

    No longer output data with missing predictor levels
    Updating function to no longer output partially-normalized data, which
    previously occurred when data were not sufficiently available to
    estimate effects of some but not all levels of model predictors. Also
    adding in default functionality to only estimate model parameters based
    on non-outlying metabolite values.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metabomxtr@125137 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mixnorm.R
+++ R/mixnorm.R
@@ -1,6 +1,6 @@
 #This function performs per-metabolite batch normalization using a mixture model with batch-specific thresholds and run order correction if desired
 
-mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTvals=NULL, removeCorrection=NULL, nNA=5,minProp=0.2,method="BFGS"){
+mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTvals=NULL, removeCorrection=NULL, nNA=5,minProp=0.2,method="BFGS", qc.sd.outliers=2){
 
 		#If not specified, indicate default mxtrModel 
 		if (is.null(mxtrModel)) mxtrModel <- as.formula(paste("~",batch,"|",batch))
@@ -18,9 +18,56 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 			data<-data.frame(cbind(t(exprs(data)), pData(data)))
 		
 		}
-
+		
+		#confirm that all variables have enough observations to run models, in both the control and observational data  
+		ynamesC<-unlist(lapply(ynames, function(metab){
+		
+			metab.vec<-cData[ , metab]
+			if( sum(!is.na(metab.vec)) > minProp*length(metab.vec)){
+			
+				metab
+			
+			} else {
+			
+				warning(paste("There is too much missing control data to proceed with normalization for", metab))
+				return(NULL)
+			
+			} 		
+		
+		}))
+		
+		ynamesO<-unlist(lapply(ynames, function(metab){
+		
+			metab.vec<-data[ , metab]
+			if( sum(!is.na(metab.vec)) > minProp*length(metab.vec)){
+			
+				metab
+			
+			} else {
+			
+				warning(paste("There is too much missing experimental data to proceed with normalization for", metab))
+				return(NULL)
+			} 		
+		
+		}))
+		
+		if (is.null(ynamesC)){
+		
+			stop("All of the input metabolites are missing too much control data to proceed with normalization.")
+		
+		} else if (is.null(ynamesO)){
+		
+			stop("All of the input metabolites are missing too much experimental data to proceed with normalization.")
+		
+		}
+		
+		ynames<-intersect(ynamesC, ynamesO)
+		
 		#confirm that all variables included in the mixture model are in both data and cData, in compatible forms 
 		model.vars<-all.vars(mxtrModelF)
+		model.var.classes<-sapply(model.vars, function(x){ class(data[ ,x]) })
+		cat.vars<-names(model.var.classes)[model.var.classes %in% c("factor", "character")]
+		
 		lapply(c(model.vars,removeCorrection),function(x){
 		
 			#make sure all variables are in both data sets
@@ -98,7 +145,7 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 	    zVarsc<-zdesign(cData,mxtrModelF)
 	    zVars<-zdesign(data,mxtrModelF)
 	    stopifnot(dim(zVarsc)[2]==dim(zVars)[2])
-	  
+				  
 	    #store order of columns in the full, control data set 
 	    full.col.order<-colnames(zVarsc)
 	    if ("(Intercept)" %in% full.col.order){
@@ -113,10 +160,58 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 	   
 	    #Find batch specific minima if batchTvals is not defined
 	    if(is.null(batchTvals)){
-		  cMin <- apply(obsYc,1,FUN=min,na.rm=TRUE)
-		  oMin <- apply(obsY,1,FUN=min,na.rm=TRUE)
-		  cMinB <- tapply(cMin,INDEX=cData[,batch],FUN=min,na.rm=TRUE)
-		  oMinB <- tapply(oMin,INDEX=data[,batch],FUN=min,na.rm=TRUE)
+		  cMin <- apply(obsYc,1, function(x){
+				
+					if (all(is.na(x))){
+					
+						NA
+					
+					} else {
+					
+						min(x, na.rm=TRUE)
+					
+					}
+				})
+		  oMin <- apply(obsY,1, function(x){
+				
+					if (all(is.na(x))){
+					
+						NA
+					
+					} else {
+					
+						min(x, na.rm=TRUE)
+					
+					}
+				})
+		  cMinB <- tapply(cMin,INDEX=cData[,batch], function(x){
+					
+					if (all(is.na(x))){
+					
+						NA
+					
+					} else {
+					
+						min(x, na.rm=TRUE)
+					
+					}		
+		  
+				})
+		  
+		  oMinB <- tapply(oMin,INDEX=data[,batch], function(x){
+					
+					if (all(is.na(x))){
+					
+						NA
+					
+					} else {
+					
+						min(x, na.rm=TRUE)
+					
+					}		
+		  
+				})
+				
 		  batchTvals <- pmin(cMinB,oMinB)
 	    }
 
@@ -127,7 +222,26 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 	    #Estimate parameters from control data to use for normalization
 	    normParams <- mxtrmod(ynames=ynames,mxtrModel=mxtrModel,Tvals=Tvals,
 							nNA=nNA,minProp=minProp,method=method,
-							data=cData)
+							data=cData, remove.outlier.sd=qc.sd.outliers)
+														
+		#determine the final metabolites for which normalization was able to run 
+		norm.metabs<-as.character(normParams[ , ".id"])
+		
+		#set outlying qc data values to Inf
+		cData[ , norm.metabs]<-apply(cData[ , norm.metabs, drop=FALSE], 2, function(qc.metab.vals){
+		
+			#determine whether the values fall outside the specified standard deviation threshold 
+			#and set to infinite if yes 
+			mean.metab.abundance<-mean(qc.metab.vals, na.rm=TRUE)
+			sd.metab.abundance<-sd(qc.metab.vals, na.rm=TRUE)
+			outlier<-qc.metab.vals > mean.metab.abundance + qc.sd.outliers*sd.metab.abundance | qc.metab.vals < mean.metab.abundance - qc.sd.outliers*sd.metab.abundance
+			outlying.values<-!is.na(qc.metab.vals) & outlier
+			qc.metab.vals[outlying.values]<-Inf
+			qc.metab.vals
+		
+		})
+		
+		#grab parameter estimates for the continuous portion of the model 		
 	    zIntI <- which(colnames(normParams)=="zInt")
 	    if(length(zIntI)==0) zIntI <- NULL
 	    normParamsZ <- normParams[,c(zIntI,grep("z_",colnames(normParams)))]
@@ -137,12 +251,86 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 	    model.conv<-normParams[ ,c(".id", "conv")]
 	  
 	    #add warning about missing data to model convergence data frame, if necessary 
-	    if ("missing_levels_warning" %in% colnames(normParams)){
+		#loop through metabolites and set values equal to infinite where model predictors are missing too much data 
+	    if ("predictors_missing_levels" %in% colnames(normParams)){
 	  
-			model.conv$missing_levels_warning<-normParams$missing_levels_warning
-			model.conv$Warning<-ifelse(is.na(model.conv$missing_levels_warning), NA, "Effects of missing levels could not be estimated and were not included in normalization.")
-	  
+			model.conv$predictors_missing_levels<-normParams$predictors_missing_levels
+			missing.level.metabs<-as.character(normParams[!is.na(normParams$predictors_missing_levels), ".id"])
+			
+			#loop over metabolites with missing levels 
+			for (metab in missing.level.metabs){
+			
+				missing.level.note<-normParams[normParams$".id"==metab, "predictors_missing_levels"]
+				missing.level.note.list<-strsplit(missing.level.note, "; ", fixed=TRUE)
+				
+				#then loop over predictors missing levels 
+				for (missing.level.note.element in missing.level.note.list){
+				
+					note.split<-strsplit(missing.level.note.element, ":", fixed=TRUE)[[1]]
+					variable<-note.split[1]
+					missing.levels<-strsplit(note.split[2], ", ", fixed=TRUE)[[1]]
+					
+					#then for each combination of metabolite and predictor level, set metabolite abundance to infinite 
+					for (missing.level in missing.levels){
+					
+							cData[cData[ , variable]==missing.level & !is.na(cData[ , metab]), metab]<-Inf
+							data[data[ , variable]==missing.level & !is.na(data[ , metab]), metab]<-Inf	
+					
+					}
+				}
+			}
 	    }
+		
+		#check for instances where an particular combination of categorical predictors is entirely missing metabolite 
+		#QC data (e.g., mother samples from batch 1), and remove data from the experimental data set if so
+		if (length(cat.vars) > 0){
+		
+			cat.var.list<-lapply(cat.vars, function(var.name) cData[ , var.name])
+			names(cat.var.list)<-cat.vars
+			cat.var.combns<-expand.grid(cat.var.list)
+			unique.cat.combns<-unique(cat.var.combns)
+		
+			for (metab in norm.metabs){
+		
+				for (row.num in 1:nrow(unique.cat.combns)){
+				
+					combn.test<-merge(cData[ , c(metab, cat.vars)], unique.cat.combns[row.num, ], by=cat.vars)
+					combn.test.target<-combn.test[!is.na(combn.test[ , metab]) & !is.infinite(combn.test[ , metab]), ]
+					if (nrow(combn.test.target)==0){
+					
+						data.copy<-data
+						data.copy$row.number<-1:nrow(data.copy)
+						remove.row.df<-merge(data.copy[ ,c(metab, cat.vars, "row.number")], unique.cat.combns[row.num, ], by=cat.vars)
+						remove.row.df<-remove.row.df[!is.na(remove.row.df[ , metab]), ]
+						problem.rows<-remove.row.df$row.number
+						
+						data[problem.rows, metab]<-Inf
+					
+					}
+				
+				}
+			}	
+		}		
+		
+		#now check for cases where a variable was excluded from a model 
+		#and set metabolite values to 0 in those instances 
+		if ("excluded_predictors" %in% colnames(normParams)){
+		
+			excluded.predictor.metabs<-normParams[!is.na(normParams$excluded_predictors), ".id"]
+			for (metab in excluded.predictor.metabs){
+			
+				cData[!is.na(cData[ , metab]), metab]<-Inf
+				data[!is.na(data[ , metab]), metab]<-Inf	
+			
+			}
+		
+			model.conv$excluded_predictors<-normParams$excluded_predictors
+				
+		}
+		
+		#recreate data frame containing response variable in control and observed data, using updated data  
+	    updated.obsYc<- yvals(cData, norm.metabs)
+	    updated.obsY <- yvals(data, norm.metabs)
 
 	    #Calculate correction values for each sample for all observed metabolites 
 	    #Don't include correction for (Intercept)
@@ -159,20 +347,35 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 		#and do not account for that factor level in normalization
 	    missing.params<-final.col.order[! final.col.order %in% colnames(normParamsZ)]
 	    na.vals<-any(apply(normParamsZ, 1, function(x) any(is.na(x))))
+		
+		#first, deal with categorical variables with too much missing data for 
+		#all input metabolites 
 	    if (length(missing.params)>0){
 	  
 		  normParamsZCopy[ , missing.params]<-0
+		  
+		  #make sure columns are in the correct order 
 		  normParamsZCopy<-normParamsZCopy[ , colnames(normParamsZCopy)[match(final.col.order, colnames(normParamsZCopy))]]
-	  
+
+		#then identify and correct cases where some but not all metabolites 
+		#are missing too much data 
 	    } else if (na.vals){
 	  
 		  for (i in 1:nrow(normParamsZCopy)){
 		
-			normParamsZCopy[i, ][is.na(normParamsZCopy[i, ])]<-0
-		
-		  }
-	  
-	    }
+			if (any(is.na(normParamsZCopy[i, ]))){
+			
+				#convert NA parameter to 0 
+				normParamsZCopy[i, is.na(normParamsZCopy[i, ])]<-0
+				
+				
+				}
+			}
+			
+			#make sure columns are in the correct order 
+			normParamsZCopy<-normParamsZCopy[ , colnames(normParamsZCopy)[match(final.col.order, colnames(normParamsZCopy))]]
+			
+		}
 	  
 	    #By default, the effects of all variables included in the mixture model 
 	    #will be subtracted from the non-normalized data. However, if desired, 
@@ -199,20 +402,21 @@ mixnorm <- function(ynames, batch="Batch", mxtrModel=NULL, cData, data, batchTva
 	   
 	    #make sure normalization parameters are in the same order as the columns in the 
 	    #obsY and obsYc datasets
-	    metab.order<-colnames(obsY)
+	    metab.order<-colnames(updated.obsY)
 	    norm.params.order<-match(metab.order, row.names(normParamsZ))
 	    normParamsZ<-normParamsZ[ norm.params.order, ]
 	    normParamsZCopy<-normParamsZCopy[ norm.params.order, ]
 	    stopifnot(all(row.names(normParamsZCopy)==metab.order))
+		stopifnot(all(colnames(normParamsZCopy)==final.col.order))
 	   
 	    #Determine correction values  		  
 	    corrValc <- zVarsc %*% t(normParamsZCopy)
 	    corrVal <- zVars %*% t(normParamsZCopy)
-
+		
 	    #Subtract correction values from observed data
-	    ctlNorm <- obsYc[,colnames(corrValc)] - corrValc 
-	    obsNorm <- obsY[,colnames(corrVal)] - corrVal 
-		  
+	    ctlNorm <- updated.obsYc[ ,colnames(corrValc)] - corrValc 
+	    obsNorm <- updated.obsY[ ,colnames(corrVal)] - corrVal
+	
 	    #return list of results 
 	    ans <- list(normParamsZ=normParamsZ, ctlNorm=ctlNorm, obsNorm=obsNorm, conv=model.conv)
 	   return(ans)
